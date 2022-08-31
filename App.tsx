05689c2c3ff085c2f08ae1e77b2dfeac0365dfae
benjamingiller <bgiller@berkeley.edu>
2022-08-31 09:27:41 -0400

    successfully connects to backend

--- App.tsx
+++ App.tsx
@@ -1,11 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Constants from "expo-constants";
+import { isDevice } from "expo-device";
 
 export default function App() {
+  interface GlucoseDataType {
+    username: string;
+    currentGlucose: number;
+  }
+
+  const [glucoseData, setGlucoseData] = useState<GlucoseDataType>({
+    username: "",
+    currentGlucose: 0,
+  });
+  const [username, setUsername] = useState("");
+
+  const localhostURL = isDevice
+    ? `http://${Constants.manifest?.debuggerHost?.split(":").shift()}:9874`
+    : "/sugars/";
+
+  const useProdUrl =
+    process.env.NODE_ENV === "production" ||
+    process.env.MIMIC_LOCALHOST_PROD === "true";
+
+  const baseURL = useProdUrl
+    ? "https://scatter-server.herokuapp.com"
+    : localhostURL;
+
+  // function getCurrentGlucose() {
+  //   fetch("http://localhost:9874/sugars/")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data);
+  //       console.log(data);
+  //     });
+  // }
+
+  function getNewGlucose() {
+    axios({
+      method: "GET",
+      url: "http://localhost:9874/sugars/",
+    })
+      .then((response) => {
+        const res = response.data;
+        setGlucoseData({
+          username: res.username,
+          currentGlucose: res.current_glucose,
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      });
+  }
+
+  useEffect(() => {
+    getNewGlucose();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        {glucoseData.username}'s' current blood sugar is
+        {glucoseData.currentGlucose}
+      </Text>
     </View>
   );
 }
@@ -13,8 +76,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
